e23a1a159495adbd23efde85252cc5056d1f7108
altescy <altescy@fastmail.com>
2021-11-03 21:54:26 +0900

    remove training job

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,4 @@
 import argparse
-import os
 
 from sagemaker.estimator import Estimator
 
@@ -7,44 +6,29 @@ from sagemaker.estimator import Estimator
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--endpoint-name", type=str, required=True)
-    parser.add_argument("-i", "--image-uri", type=str, required=True)
-    parser.add_argument("-r", "--execution-role", type=str, required=True)
-    parser.add_argument("--dataset-path", type=str, default=None)
-    parser.add_argument("--artifact-path", type=str, default=None)
-    parser.add_argument("--training-instance", type=str, default="ml.m5.large")
-    parser.add_argument("--inference-instance", type=str, default="ml.t2.medium")
+    parser.add_argument("-t", "--training-job", type=str, required=True)
+    parser.add_argument("--instance-type", type=str, default="ml.m5.large")
+    parser.add_argument("--instance-count", type=int, default=1)
     parser.add_argument("--local", action="store_true")
     args = parser.parse_args()
 
-    role = args.execution_role
-    image_uri = args.image_uri
     endpoint_name = args.endpoint_name
+    training_job_name = args.training_job
 
     if args.local:
-        training = f"file://{os.path.abspath('./data')}"
-        output = f"file://{os.path.abspath('./data')}"
-        training_instance_type = "local"
-        inference_instance_type = "local"
+        instance_type = "local"
+        instance_type = "local"
+        instance_count = 1
     else:
-        training = args.dataset_path
-        output = args.artifact_path
-        training_instance_type = args.training_instance
-        inference_instance_type = args.inference_instance
+        instance_type = args.instance_type
+        instance_count = args.instance_count
 
-    estimator = Estimator(
-        image_uri=image_uri,
-        role=role,
-        instance_count=1,
-        instance_type=training_instance_type,
-        output_path=output,
-    )
-
-    estimator.fit({"training": training})
+    estimator = Estimator.attach(training_job_name)
 
     estimator.deploy(
         endpoint_name=endpoint_name,
-        instance_type=inference_instance_type,
-        initial_instance_count=1,
+        instance_type=instance_type,
+        initial_instance_count=instance_count,
     )
 
 
